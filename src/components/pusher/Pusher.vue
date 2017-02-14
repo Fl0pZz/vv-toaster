fd1b18d8a4998bf53323cc78243b5f4aceae2d4b
Fakhreev Rafael <fahreevrafael@yandex.ru>
2017-02-15 01:07:59 +0300

    add push test component

---
+++ src/components/pusher/Pusher.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+    <div>
+        <a @click="pushI()">Отправить уведомление</a>
+        <a @click="pushE()">Отправить ошибку</a>
+    </div>
+</template>
+
+<script lang="babel">
+  import { pushInfo, pushError } from 'store/notifications/notification-wrappers'
+
+  export default{
+    name: 'pusher',
+    methods: {
+      pushI () {
+        const messages = [{ header: 'INFO', message: 'Hello world!' }]
+        pushInfo(messages)
+      },
+      pushE () {
+        const messages = [{ header: 'ERROR', message: 'Error world!' }]
+        pushError(messages)
+      }
+    }
+  }
+</script>
