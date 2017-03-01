fc6226cb80ea195e71e30069c5453be23900a497
Fakhreev Rafael <fahreevrafael@yandex.ru>
2017-03-02 00:03:43 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,22 +1,63 @@
-import Toaster from './components/default/toastmanager/Toaster.vue'
 import notification from './store/notifications/index'
 import * as nTypes from './store/notifications/notification-types'
 import * as nActionTypes from './store/notifications/notification-types'
+import ToasterManager from './components/default/toastmanager/Toaster.vue'
+
+export default class Toaster {
+  constructor(store, namespaced = false) {
+    this._store = store
+    this._prefix = namespaced ? 'notification/' : ''
+  }
+
+  install (Vue) {
+    Vue.prototype.$toaster = this
+  }
+
+  pushNotice (notification) {
+    return this._store.dispatch(this._prefix + 'push', notification)
+  }
+
+  pushInfo (messages, action = nActionTypes.NO) {
+    if (typeof messages === 'string' || messages instanceof String)
+      messages = [{ header: nTypes.INFO, message: messages }]
+    if (!(messages instanceof Array))
+      messages = [messages]
+    return this.pushNotice({type: nTypes.INFO, messages, action})
+  }
+
+  pushError (messages, action = nActionTypes.CLOSE) {
+    if (typeof messages === 'string' || messages instanceof String)
+      messages = [{ header: nTypes.ERROR, message: messages }]
+    if (!(messages instanceof Array))
+      messages = [messages]
+    return this.pushNotice({ type: nTypes.ERROR, messages, action })
+  }
+
+  updateNotice (id, notification) {
+    return this._store.dispatch(this._prefix + 'update', { id, notification })
+  }
+
+  updateInfo (id, messages, action = nActionTypes.NO) {
+    if (typeof messages === 'string' || messages instanceof String)
+      messages = [{ header: nTypes.INFO, message: messages }]
+    if (!(messages instanceof Array))
+      messages = [messages]
+    return this.updateNotice(id, {type: nTypes.INFO, messages, action})
+  }
+
+  updateError (id, messages, action = nActionTypes.CLOSE) {
+    if (typeof messages === 'string' || messages instanceof String)
+      messages = [{ header: nTypes.ERROR, message: messages }]
+    if (!(messages instanceof Array))
+      messages = [messages]
+    return this.updateNotice(id, {type: nTypes.ERROR, messages, action})
+  }
+}
 
 export {
   notification,
   Toaster,
   nTypes,
-  nActionTypes
-}
-
-export default {
-  Toaster,
-  install (Vue, toaster) {
-    Vue.prototype.$toaster = toaster
-  },
-  notification,
-  nTypes,
-  nActionTypes
-
+  nActionTypes,
+  ToasterManager
 }
